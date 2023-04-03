dd82cb53553a8a0e3e906385984255be17f6a7f1
EmiLef08 <emilef08@gmail.com>
2023-04-04 00:01:32 +0200

    file presentation project + start header and homepage + create database + import photos and pictures

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
+import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
