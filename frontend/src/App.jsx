cc7c520cfdcc54fa0ff857dc9121f1d982ac3922
EmiLef08 <emilef08@gmail.com>
2023-04-04 20:45:33 +0200

    Desk navbar created, footer desk on process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import MobNavbar from "./components/MobNavbar";
+import DeskNavbar from "./components/DeskNavbar";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
@@ -11,10 +13,12 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <DeskNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <MobNavbar />
+      <Footer />
     </div>
   );
 }
