4dafe15e25cd82d56e7d0bb01a3dc4a8fc23d8c3
EmiLef08 <emilef08@gmail.com>
2023-04-04 18:23:07 +0200

    Home page created, form non-fonctionnel, liens fonctionnels

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 // eslint-disable-next-line import/no-extraneous-dependencies
 // import axios from "axios";
+import { LogoFacebook, LogoInstagram } from "react-ionicons";
 import Dijon from "../assets/Dijon.jpeg";
-import banques from "../assets/banques_2.png";
+import banques from "../assets/banques_2.jpg";
 import cabaret from "../assets/Cabaret_2.png";
-import cnl from "../assets/cnl_vannes_2.png";
+import cnl from "../assets/cnl_vannes_2.jpg";
 
 import styles from "../styles/pages/Home.module.scss";
 
@@ -46,6 +47,10 @@ export default function Home() {
     navigate("/information");
   };
 
+  const handleClickActions = () => {
+    navigate("/actions");
+  };
+
   return (
     <div className={styles.home}>
       <div className={styles["home-pict"]}>
@@ -79,15 +84,95 @@ export default function Home() {
         <img src={banques} alt="Banques Alimentaires" />
         <img src={cnl} alt="Convention Nationale LEO Vannes" />
       </div>
-      {/* <div className={styles["home-btn"]}>
+      <div className={styles["home-btn"]}>
         <button
           type="button"
-          className={styles["btn-a-propos"]}
-          onClick={handleClick}
+          className={styles["btn-actions"]}
+          onClick={handleClickActions}
         >
-          EN SAVOIR +
+          NOS ACTIONS
         </button>
-      </div> */}
+      </div>
+      <div className={styles["home-contact"]}>
+        <div className={styles.reseaux}>
+          <a href="https://www.facebook.com/LeoClubReims">
+            <LogoFacebook color="#ffffff" height="3rem" width="3rem" />
+          </a>
+          <h2>Suivez-nous sur nos réseaux !</h2>
+          <a href="https://www.instagram.com/leoclubreims/">
+            <LogoInstagram color="#ffffff" height="3rem" width="3rem" />
+          </a>
+        </div>
+        <div className={styles.contact}>
+          <div className={styles.form}>
+            <h2>Nous contacter ?</h2>
+            <form className={styles["contact-form"]}>
+              <label htmlFor="name" className={styles["label-contact"]}>
+                Nom et prénom :
+                <input
+                  className={styles["input-contact"]}
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Nom et prénom"
+                  required
+                />
+              </label>
+              <label htmlFor="email" className={styles["label-contact"]}>
+                Email :
+                <input
+                  className={styles["input-contact"]}
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  required
+                />
+              </label>
+              <label htmlFor="téléphone" className={styles["label-contact"]}>
+                Téléphone :
+                <input
+                  className={styles["input-contact"]}
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  placeholder="Téléphone"
+                  required
+                />
+              </label>
+              <label htmlFor="sujet" className={styles["label-contact"]}>
+                Sujet :
+              </label>
+              <select
+                name="sujet"
+                id="sujet"
+                className={styles["subject-contact"]}
+              >
+                <option selected>Sélectionnez l'objet de votre message</option>
+                <option value="member">Devenir membre</option>
+                <option value="actions">Partenariat pour une action</option>
+                <option value="information">Demande d'information</option>
+                <option value="other">Autre sujet</option>
+              </select>
+              <label htmlFor="message" className={styles["label-contact"]}>
+                Message :
+                <textarea
+                  className={styles["message-contact"]}
+                  name="message"
+                  id="message"
+                  cols="30"
+                  rows="10"
+                  placeholder="Message"
+                  required
+                />
+              </label>
+              <button type="submit" className={styles["btn-contact"]}>
+                Envoyer
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
